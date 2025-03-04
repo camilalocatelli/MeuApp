084e929bf48ac09792956b419ad1e8fc77910738
Cami <Cami@CAMI>
2025-03-04 19:21:18 -0300

    Adicionar arquivos de projeto.

---
+++ MeuApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MeuApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MeuApp/View/ListaProduto.xaml.cs
@@ -0,0 +1,9 @@
+namespace MeuApp.View;
+
+public partial class ListaProduto : ContentPage
+{
+	public ListaProduto()
+	{
+		InitializeComponent();
+	}
+}
