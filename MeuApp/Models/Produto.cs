96c7a2a30e84afe8664c1813e94cfeb95fac5b84
Cami <Cami@CAMI>
2025-03-22 14:39:11 -0300

    Descrição do produto obrigatório e correção de produtos duplicados na tela inicial

--- MeuApp/Models/Produto.cs
+++ MeuApp/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,22 @@ namespace MeuApp.Models
 {
     public class Produto
     {
+        string _descricao;
 
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public string Descricao { 
+            get => _descricao; 
+            set
+            {
+                if(value == null)
+                {
+                    throw new Exception("Descrição não pode ser vazia");
+                }
+
+                _descricao = value;
+            }
+        }
         public double Quantidade { get; set; }
         public double Preco { get; set; }
         public double Total { get => Quantidade * Preco; }
